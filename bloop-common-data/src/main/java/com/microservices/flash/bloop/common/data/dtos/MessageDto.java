2436f460371bfa18f63a3b910c18b8b8813538bb
Tafadzwa Mandava <papacita@Tafadzwas-MacBook-Pro.local>
2022-09-10 15:49:38 +0200

    Adding data annotations to Message DTO

--- bloop-common-data/src/main/java/com/microservices/flash/bloop/common/data/dtos/MessageDto.java
+++ bloop-common-data/src/main/java/com/microservices/flash/bloop/common/data/dtos/MessageDto.java
@@ -2,6 +2,15 @@ package com.microservices.flash.bloop.common.data.dtos;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MessageDto {
 
     private UUID id;
